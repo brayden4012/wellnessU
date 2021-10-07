90659036b2c2c982ba805e5d99af67eb3a1e8e91
Brayden Harris <brayden.harris@bottlerocketstudios.com>
2021-10-07 13:28:14 -0500

    Initial Commit

---
+++ wellnessU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wellnessU
+//
+//  Created by Brayden Harris on 10/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wellnessU/wellnessUApp.swift
@@ -0,0 +1,17 @@
+//
+//  wellnessUApp.swift
+//  wellnessU
+//
+//  Created by Brayden Harris on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct wellnessUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
